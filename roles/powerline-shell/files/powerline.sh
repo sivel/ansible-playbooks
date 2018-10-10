bf62742ee983503f0edc30ceb09fb6d1b7af0c4d
Matt Martz <matt@sivel.net>
2018-10-10 13:49:43 -0500

    Further enhancements to remote tool install

--- roles/powerline-shell/files/powerline.sh
+++ roles/powerline-shell/files/powerline.sh
@@ -1,3 +1,5 @@
+export PROMPT_COMMAND='history -a; history -n; shopt -s histappend; printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"'
+
 function _update_ps1() {
     export PS1="$(${HOME}/bin/powerline_shell bash)"
 }
--- roles/pyenv/files/pyenv.sh
+++ roles/pyenv/files/pyenv.sh
@@ -1,5 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+export CFLAGS="-O2"
 
 if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
