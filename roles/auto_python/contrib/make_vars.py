f8c57de00d055f5eab32533f2500fc955786c780
Matt Martz <matt@sivel.net>
2021-08-31 12:24:03 -0500

    Example alternative to interpreter_discovery

---
+++ roles/auto_python/contrib/make_vars.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# (c) 2021, Matt Martz <matt@sivel.net>
+# GNU General Public License v3.0+
+#     (see https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import os
+
+from ansible import config
+
+import yaml
+
+base = os.path.join(
+    os.path.dirname(config.__file__),
+    'base.yml'
+)
+
+with open(base) as f:
+    data = yaml.safe_load(f)
+
+vars_data = {
+    '_d_distro_map': data['INTERPRETER_PYTHON_DISTRO_MAP']['default'],
+    '_d_py_fallback': data['INTERPRETER_PYTHON_FALLBACK']['default'],
+}
+
+with open('vars/main.yml', 'w+') as f:
+    yaml.dump(vars_data, f, indent=2, default_flow_style=False)
